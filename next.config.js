acc44913adb333af0f756e2c33aee9f4dc10c634
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-05-19 20:41:01 +0100

    Removing css modules from next.confi.js, changing location of title, meta description

--- next.config.js
+++ next.config.js
@@ -14,5 +14,5 @@ module.exports = {
     return config;
   },
   // ...
-  cssModules: true,
+  // cssModules: true,
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Listencraft</title>
-        <meta
-          name="description"
-          content="Listencraft is the app to teach people how to listen"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
